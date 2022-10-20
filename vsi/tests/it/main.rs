743d8a294a14fb605681b2ecb450340c35e3896a
Wes Van Melle <van.melle.wes@gmail.com>
2022-10-20 11:36:11 -0700

    Tests (stolen from archive tests)

--- vsi/tests/it/main.rs
+++ vsi/tests/it/main.rs
@@ -1,9 +1,16 @@
 //! Integration tests.
 
+use std::collections::HashMap;
+use std::sync::Arc;
 use std::{collections::HashSet, env};
 
+use async_trait::async_trait;
+use stable_eyre::eyre::ensure;
 use stable_eyre::{eyre::Context, Result};
+use tokio::sync::Mutex;
+
 use vsi::config;
+use vsi::scan::{Artifact, Id, Options, Sink};
 
 mod runner;
 
@@ -40,6 +47,74 @@ async fn run_succeeds() -> Result<()> {
 
     // Currently the criteria for success for this test is very very basic.
     assert!(result.contains("folly") && result.contains("tesseract"));
+
+    Ok(())
+}
+
+struct MemorySink {
+    // We don't care about performance here, mutex is not a concern.
+    buffer: Arc<Mutex<Vec<Artifact>>>,
+    id: Id,
+}
+
+#[async_trait]
+impl Sink for MemorySink {
+    async fn append_scan(&self, id: &Id, artifacts: Vec<Artifact>) -> Result<()> {
+        ensure!(id == &self.id, "id's did not match: {} <=> {}", id, self.id);
+        self.buffer.lock().await.extend(artifacts);
+        Ok(())
+    }
+}
+
+#[tokio::test]
+async fn archive_scan_produces_correct_prints() -> Result<()> {
+    let id = Id::new("Fake ID, I promise I'm 21.");
+    let sink = MemorySink {
+        buffer: Arc::new(Mutex::new(Vec::new())),
+        id: id.clone(),
+    };
+    let opts = Options::builder()
+        .root("tests/it/testdata/archive-scan")
+        .build();
+    let count = vsi::scan::artifacts(&sink, &id, opts)
+        .await
+        .context("vsi scan")?;
+    // two files in zip, plus the zip itself
+    assert_eq!(count, 3, "count of produced artifacts");
+
+    let results = Arc::try_unwrap(sink.buffer)
+        .expect("unwrap arc")
+        .into_inner();
+    assert_eq!(
+        count,
+        results.len(),
+        "reported count does not match sink ingestion count"
+    );
+
+    let result_map: HashMap<_, _> = results
+        .into_iter()
+        .map(|art| {
+            let (key, comb) = art.explode_string();
+            (key, comb.to_string())
+        })
+        .collect();
+
+    let simple_actual = result_map.get("simple.zip").map(|s| s.as_str());
+    let simple_zip_expected =
+        "sha_256(65edda9e1933aa8cff1d5aeec70a8ddbd43f971454b982f101aa9beff0b72901)";
+    assert_eq!(simple_actual, Some(simple_zip_expected));
+
+    let a_actual = result_map
+        .get("simple.zip!_fossa.virtual_!/simple/a.txt")
+        .map(|s| s.as_str());
+    let a_expected = "sha_256(a1521f679d5583c4bac29209c655c04a6cadb68a364d448d7b43224aeffd82ce); comment_stripped:sha_256(a1521f679d5583c4bac29209c655c04a6cadb68a364d448d7b43224aeffd82ce)";
+    assert_eq!(a_actual, Some(a_expected));
+
+    let b_actual = result_map
+        .get("simple.zip!_fossa.virtual_!/simple/b.txt")
+        .map(|s| s.as_str());
+    let b_expected = "sha_256(367a5b6e6b67fa0c2d00dee7c91eb3f0d85a93e537335abbed7908c9f87738c8); comment_stripped:sha_256(367a5b6e6b67fa0c2d00dee7c91eb3f0d85a93e537335abbed7908c9f87738c8)";
+    assert_eq!(b_actual, Some(b_expected));
 
     Ok(())
 }
