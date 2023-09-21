34fee597b588248b6c80a1519b35be7ab39864fd
Christopher Sasarak <chris@fossa.com>
2023-09-20 23:52:42 -0400

    Use SnippetContext

--- snippets/src/language/normalize_comments.rs
+++ snippets/src/language/normalize_comments.rs
@@ -1,33 +1,19 @@
 use crate::tree_sitter_consts::NODE_KIND_COMMENT;
 use std::borrow::Cow;
-use tree_sitter::Node;
+
+use super::c99_tc3::SnippetContext;
 
 /// Remove all comment node text from the given content.
 /// In general, this function should work in any language that produces "comment" type nodes from tree_sitter.
 #[tracing::instrument(skip_all)]
-pub fn normalize_comments<'a, 'b>(
-    context: &[Node<'_>],
-    location: &crate::Location,
-    content: &'a [u8],
+pub fn normalize_comments<'a>(
+    context: &SnippetContext
 ) -> Cow<'a, [u8]> {
-    let comment_nodes = context
+    let comment_nodes = context.context_nodes()
         .into_iter()
         .filter(|n| n.kind() == NODE_KIND_COMMENT);
-    let crate::ByteOffset(offset) = location.byte_offset;
-    let mut start_byte = 0;
-    let mut slices = Vec::new();
 
-    // Find every non-comment section of text from the original content into a sequence
-    for node in comment_nodes {
-        let end_byte = node.start_byte() - offset;
-        let next_start_byte = node.end_byte() - offset;
-        slices.push(&content[start_byte..end_byte]);
-        start_byte = next_start_byte;
-    }
-
-    slices.push(&content[start_byte..content.len()]);
-
-    slices.concat().into()
+    context.retrieve_negative_content(comment_nodes).collect::<Vec<_>>().concat().into()
 }
 
 mod tests {
