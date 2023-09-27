d106ab52658bd53b1d4b2c0ee0d42e5cbe322c78
Christopher Sasarak <chris@fossa.com>
2023-09-27 17:13:33 -0400

    Initial tests.

---
+++ snippets/tests/it/language/testdata/cpp_98/bare_functions.cc
@@ -0,0 +1,13 @@
+// Your First C++ Program
+
+#include <iostream>
+
+namespace hello_world {
+  
+  int main(/* A comment */) {
+      std::cout << "Hello World!";
+      return 0;
+      // another comment
+  }
+
+}
